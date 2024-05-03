d932d83bd8a9418fc43182203236ac428d5ce367
Vishnu tyagi <vishnu.tyagi.mec20@itbhu.ac.in>
2024-05-04 01:17:52 +0530

    added the weather api

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -11,11 +11,37 @@ import humidity_icon from '../Assets/humidity.png'
 
 
 const WeatherApp = () => {
+   
+    let api_key="330e444f0429928f3558171737300df3";
+
+    const search = async()=>{
+        const element=document.getElementsByClassName("cityInput")
+        if(element[0].value===""){
+            return 0;
+        }
+        let url=`https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
+        
+        let response = await fetch(url);
+        let data = await response.json();
+        const humidity = document.getElementsByClassName("humidity-percent");
+        const wind = document.getElementsByClassName("wind-rate");
+        const temperature = document.getElementsByClassName("weather-temp");
+        const location = document.getElementsByClassName("weather-location");
+
+        humidity[0].innerHTML  = data.main.humidity+' %';
+        wind[0].innerHTML = data.wind.speed+" Km/h";
+        temperature[0].innerHTML = data.main.temp+"°C"
+        location[0].innerHTML = data.name;
+
+    }
+
+
+
   return (
     <div className='container'>
       <div className='top-bar'>
           <input type='text' className='cityInput' placeholder='Search'/>        
-           <div className='search-icon'>
+           <div className='search-icon' onClick={()=>{search()}} >
                  <img src={search_icon} alt="" />
            </div>
            <div className='weather-image'>
@@ -34,7 +60,7 @@ const WeatherApp = () => {
             <div className='element'>
                 <img  src={wind_icon} alt="" className='icon'/>
                 <div className='data'>
-                    <div className='humidity-percent'>64%</div>
+                    <div className='wind-rate'>64%</div>
                      <div className='text'>Wind Speed</div>   
                 </div>
             </div>
