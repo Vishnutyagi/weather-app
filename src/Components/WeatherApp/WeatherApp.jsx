616abec54561ecdb270f99b07309e57d877ae085
Vishnu tyagi <vishnu.tyagi.mec20@itbhu.ac.in>
2024-05-04 01:31:24 +0530

    added weather-icon

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './WeatherApp.css'
 import search_icon from '../Assets/search.png'
 import clear_icon from '../Assets/clear.png'
@@ -13,6 +13,8 @@ import humidity_icon from '../Assets/humidity.png'
 const WeatherApp = () => {
    
     let api_key="330e444f0429928f3558171737300df3";
+
+    const [wicon,setWicon] = useState(cloud_icon);
 
     const search = async()=>{
         const element=document.getElementsByClassName("cityInput")
@@ -32,6 +34,32 @@ const WeatherApp = () => {
         wind[0].innerHTML = data.wind.speed+" Km/h";
         temperature[0].innerHTML = data.main.temp+"°C"
         location[0].innerHTML = data.name;
+        
+        if(data.weather[0].icon==="01d" || data.weather[0].icon === "01n"){
+            setWicon(clear_icon);
+        }
+        else if (data.weather[0].icon==="02d" || data.weather[0].icon === "02n"){
+            setWicon(cloud_icon);
+        }
+        else if (data.weather[0].icon==="03d" || data.weather[0].icon === "03n"){
+            setWicon(drizzle_icon);
+        }
+        else if (data.weather[0].icon==="04d" || data.weather[0].icon === "04n"){
+            setWicon(drizzle_icon);
+        }
+        else if (data.weather[0].icon==="09d" || data.weather[0].icon === "09n"){
+            setWicon(rain_icon);
+        }
+        else if (data.weather[0].icon==="10d" || data.weather[0].icon === "1n"){
+            setWicon(cloud_icon);
+        }
+        else if (data.weather[0].icon==="13d" || data.weather[0].icon === "13n"){
+            setWicon(snow_icon);
+        }
+        else{
+            setWicon(clear_icon); 
+        }
+
 
     }
 
@@ -45,7 +73,7 @@ const WeatherApp = () => {
                  <img src={search_icon} alt="" />
            </div>
            <div className='weather-image'>
-             <img src={cloud_icon} alt="" />
+             <img src={wicon} alt="" />
            </div>
            <div className='weather-temp'> 24*c</div>
            <div className='weather-location'>London</div>
